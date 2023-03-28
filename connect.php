5de6eed417f2f310e676cc48a8ae7d7c1a2f9aa6
Pome <114458716+pomegranata@users.noreply.github.com>
2023-03-29 04:33:38 +0700

    code

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@
 	$db = mysqli_connect($server, $user, $password, $nama_database);
 
 	if(!$db){
-		die("Gagal terhubung dengan database: " . mysqli_connect_error());
+		die("Failed to connect: " . mysqli_connect_error());
 	}
 
 ?>
